f20c1354b89232fd5e76b2fee8b6a371f52dda74
Snicket <snicket.knight@gmail.com>
2017-12-07 12:53:43 -0600

    Added secret commands

--- src/game/Main.java
+++ src/game/Main.java
@@ -69,7 +69,7 @@ public class Main {
 				System.out.println("Type 'look closer' for a closer look at items in the room");
 				System.out.println("Type 'take <item>' to take the item you inputed");
 				System.out.println("Type 'backpack' to look in your bag of trash");
-				System.out.println("There are some commands that can only be accessed by obtaining a special item");
+				System.out.println("There are some commands that can only be accessed by trial and error");
 				System.out.println("------------------------------------------------------");
 
 			}
@@ -100,7 +100,12 @@ public class Main {
 			{
 				player.printInventory();
 			}
-		
+
+			else if (input.equals("memes"))
+			{
+				System.out.println("Many people always ask me how I was able to get into Harvard as a 16 year old who skipped 3 grades of high school. They think I got in because of my scholarly records, but no the key is the interview.\n" +
+						"As I sat in the Harvard Dean's office in front of the board of reviewers for my application, the Dean asks me \"Why should you be a good candidate for this school?\" They seemed bored but I replied \"Well I was born a child prodigy, placed 1st in my state spelling bee for three consecutive years, I can speak eight different languages not counting Latin, play four different instruments, I skipped grades 4 through 6, and graduated my high school as valedictorian at the age of 14. I then worked as an intern at both Telsa, and NASA.\" Suddenly the room burst into laughter and many of board instantly started scribbling down \"No\" near the application check marks. The Dean says \"Sorry but you are just not the type we are looking for.\" But then I said \"Excuse me but I wasn't finished... I watch Rick and Morty\" The Dean looked at me like an idiot and said \"So....?\" Then I replied with a smile \"And I understand all the references and subtle jokes\" An audible gasp let out by the board was so loud the secretary had to come in. You could hear a pin drop and then suddenly all at once the entire board clicked their pens on the \"Approved Box\" and I was instantly handed a diploma and now I'm teaching advanced physicals there. I guess you can say I'm pretty smart. :)");
+			}
 			// Quit
 			else if (input.equals("quit")) {
 				System.out.println("Thank you for playing!");
